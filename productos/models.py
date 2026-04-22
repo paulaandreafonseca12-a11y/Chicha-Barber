6541c771015fe6d754174afe7719f8fa5f4414d7
nataliaacero37-blip <nataliaacero37@gmail.com>
2026-04-22 12:14:03 -0500

    feat: se corrige registrar compras

--- productos/models.py
+++ productos/models.py
@@ -38,7 +38,6 @@ class Producto(models.Model):
 
 class Compra(models.Model):
     codigo_compra = models.AutoField(primary_key=True)
-    # Datos del cliente
     nombre_cliente = models.CharField(max_length=100, verbose_name="Nombre del Cliente")
     correo = models.EmailField(verbose_name="Correo", blank=True, null=True)
     telefono = models.CharField(max_length=20, verbose_name="Teléfono", blank=True, null=True)
@@ -58,35 +57,10 @@ class DetalleCompra(models.Model):
     subtotal = models.DecimalField(max_digits=10, decimal_places=2, editable=False)
 
     def save(self, *args, **kwargs):
-        self.subtotal = self.cantidad * self.producto.precio_venta  # ← precio_venta, no precio_compra
+        self.subtotal = self.cantidad * self.producto.precio_venta
         super().save(*args, **kwargs)
         self.compra.total = sum(d.subtotal for d in self.compra.detalles.all())
         self.compra.save()
 
     def __str__(self):
         return f"{self.producto.codigo} x {self.cantidad}"
-
-
-class Pago(models.Model):
-    compra = models.ForeignKey(
-        Compra,
-        on_delete=models.CASCADE,
-        related_name="pagos"
-    )
-
-    METODOS_PAGO = [
-        ('persona', 'Pago en persona'),
-        ('contraentrega', 'Pago contraentrega'),
-    ]
-
-    nombre = models.CharField(max_length=100)
-    correo = models.EmailField()
-    telefono = models.CharField(max_length=20)
-    direccion = models.CharField(max_length=255)
-
-    metodo_pago = models.CharField(max_length=20, choices=METODOS_PAGO)
-
-    fecha = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"Pago de {self.nombre} - Compra {self.compra.codigo_compra}"
