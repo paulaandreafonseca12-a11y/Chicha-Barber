ae38de929a5c8282f802076ec5f4253b1e5a397e
paulaandreafonseca12-a11y <paulaandreafonseca12@gmail.com>
2026-04-22 15:33:21 -0500

    feat: actualizar enlaces y mejorar la gestión de servicios en las plantillas

--- servicios/urls.py
+++ servicios/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('crear/', views.crear_servicios, name='crear_servicios'),
 
     # Calificación → /servicios/calificacion/
-    path('calificacion/', views.calificacion_views, name='calificacion'),
+
 
     # Promociones → /servicios/promocion/
     path('promocion/', views.promocion, name='promocion'),
@@ -18,9 +18,11 @@ urlpatterns = [
     path('crear-promocion/', views.crear_promocion, name='crear_promocion'),
 
     # Seleccionar promoción
-    path(
-        'promocion/seleccionar/<str:nombre_promo>/',
-        views.seleccionar_promocion,
-        name='seleccionar_promocion'
-    ),
+    path('editar/<int:pk>/', views.editar_servicios, name='editar-servicios'),
+    path('eliminar/<int:pk>/', views.eliminar_servicios, name='eliminar-servicios'),
+    path('listado/', views.listado_admin, name='listado-admin'),
+    path('promocion/listado/', views.listado_promocion, name='listado-promocion'),
+    path('promocion/editar/<int:pk>/', views.editar_promocion, name='editar-promocion'),
+    path('promocion/eliminar/<int:pk>/', views.eliminar_promocion, name='eliminar-promocion'),
+    
 ]
