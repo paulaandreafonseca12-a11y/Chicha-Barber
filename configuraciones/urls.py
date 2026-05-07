c13206d88d7cc9fbffb6e696b599f15773dfdb51
paulaandreafonseca12-a11y <paulaandreafonseca12@gmail.com>
2026-05-07 12:34:30 -0500

    feat: cambios en configuraciones

--- configuraciones/urls.py
+++ configuraciones/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('carrusel/', views.carrusel_view, name='carrusel'),
     path('carrusel/crear/', views.crear_carrusel, name='crear_carrusel'),
+    path('carrusel/editar/<int:pk>/', views.editar_carrusel, name='editar_carrusel'),
+    path('carrusel/eliminar/<int:pk>/', views.eliminar_carrusel, name='eliminar_carrusel'),
 ]
