8de0dc255e6cda5557cd70a7614a6a2836a6b6a3
paulaandreafonseca12-a11y <paulaandreafonseca12@gmail.com>
2026-06-05 11:39:17 -0500

    fix: cambios en promocion servicios y inplementacionde archivos  js

---
+++ static/js/tconf/tconf-tablaCarrusel.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+    $('#tablaCarrusel').DataTable({
+        "language": {
+            "sProcessing": "Procesando...",
+            "sLengthMenu": "Mostrar _MENU_ registros",
+            "sZeroRecords": "No se encontraron resultados",
+            "sEmptyTable": "Ningún dato disponible en esta tabla",
+            "sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+            "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+            "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
+            "sInfoPostFix": "",
+            "sSearch": "Buscar:",
+            "sUrl": "",
+            "sInfoThousands": ",",
+            "sLoadingRecords": "Cargando...",
+            "oPaginate": {
+                "sFirst": "Primero",
+                "sLast": "Último",
+                "sNext": "Siguiente",
+                "sPrevious": "Anterior"
+            },
+            "oAria": {
+                "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
+                "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+            }
+        },
+        "paging": true,
+        "lengthChange": true,
+        "searching": true,
+        "ordering": true,
+        "info": true,
+        "autoWidth": false,
+        "responsive": true,
+        "columnDefs": [
+            { "orderable": false, "targets": [0, 2] }, // Imagen y Acciones
+            { "searchable": false, "targets": [0, 2] } // Imagen y Acciones
+        ]
+    });
+});
