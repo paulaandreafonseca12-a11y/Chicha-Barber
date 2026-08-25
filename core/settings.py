02e90db4c3a870f67ff3e9f9d9f7558374cf915d
dasantiagogarciatorres-glitch <dasantiagogarciatorres@gmail.com>
2026-08-25 11:36:08 -0500

    fix: corrección de pasword en setting y .txt

--- core/settings.py
+++ core/settings.py
@@ -93,10 +93,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'bdchichabarber',
         'USER': 'root',       
-        'PASSWORD': '1234567',      
+        'PASSWORD': 'S100253726925s',      
         'HOST': '127.0.0.1',             
-        'PORT': '3306',                 
-        'OPTIONS': {
+        'PORT': '3306', 'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         },
     }
